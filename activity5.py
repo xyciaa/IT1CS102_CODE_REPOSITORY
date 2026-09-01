b4d89cc33924aff381427e472e94a4018b352863
cia <atltrixie7@gmail.com>
2026-09-01 13:04:14 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,11 @@
+a= 14
+b= 7
+c=81
+d=5
+print('The sum of',a,'and',b,'is',a+b)
+print('The difference of',a,'and',b,'is',a-b)
+print('The product of',a,'and',b,'is',a*b)
+print('The quotient of',a,'and',b,'is',a/b)
+print('The round down quotient of',a,'and',b,'is',a//b)
+print('The exponent of',c,'and',d,'is',c**d)
+print('The remainder of',c,'and',d,'is',c%d)
---
+++ activity6.py
@@ -0,0 +1,3 @@
+name = input('Hello what is your name? -->')
+
+print('Hi,',name,',how are you?')
